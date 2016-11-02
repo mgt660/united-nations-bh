7710efc29306b454da60205d1b1345704e00f083
MGT 660 <mgt660.2016@gmail.com>
2016-11-02 20:32:40 +0000

    First commit hopefully

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('United Nations interactive website under construction');
 });
-
+app.get('/netid', function(request, response){
+    response.send('what do you mean');
+});
 app.listen(process.env.PORT || 4000);
