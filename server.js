13769a38fc9f9ef97baa4dd27a6c78eb9da6094f
Nicholas D'Andre <nicholasmdandre@gmail.com>
2016-11-11 15:31:08 +0000

    updated server with internal links

--- server.js
+++ server.js
@@ -6,7 +6,28 @@ app.use(express.static('static'))
 app.get('/', function(request, response){
     response.sendfile('index.html');
 });
-app.get('/netid', function(request, response){
-    response.send('what do you mean');
+app.get('/algeria', function(request, response){
+    response.send('algeria.html');
+});
+app.get('/chad', function(request, response){
+    response.send('chad.html');
+});
+app.get('/colombia', function(request, response){
+    response.send('colombia.html');
+});
+app.get('/indonesia', function(request, response){
+    response.send('indonesia.html');
+});
+app.get('/nigeria', function(request, response){
+    response.send('nigeria.html');
+});
+app.get('/somalia', function(request, response){
+    response.send('somalia.html');
+});
+app.get('/southafrica', function(request, response){
+    response.send('southafrica.html');
+});
+app.get('/srilanka', function(request, response){
+    response.send('srilanka.html');
 });
 app.listen(process.env.PORT || 4000);
