9e111c0533c7ef7f310e2845a408efd5449b9abe
Er Dem <erdem.erder@yale.edu>
2016-11-16 20:06:55 +0000

    Added dummy reports and updated server.js and package.json

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+
 
 app.get('/', function(request, response){
     response.send('United Nations interactive website under construction');
 });
+app.get('/start', function(request, response){
+    response.sendFile(path.join(__dirname + '/Erdem_1.html'));
+});
 app.get('/netid', function(request, response){
     response.send('what do you mean');
+});
+app.get('/colombia', function(request, response){
+    response.sendFile(path.join(__dirname + '/reports' + '/colombia.html'));
+});
+app.get('/indonesia', function(request, response){
+    response.sendFile(path.join(__dirname + '/reports' + '/indonesia.html'));
 });
 app.listen(process.env.PORT || 4000);
