e7132ab4c9295dd4ca65bac2f7322c31574edc40
MGT 660 <mgt660.2016@gmail.com>
2016-12-06 05:15:32 +0000

    fix-index.js

--- index.js
+++ index.js
@@ -1,63 +1,65 @@
 $(document).ready(function() {
-	
+
+/global $*/
+
   $( "#home" ).click( function() {
     window.location = "index.html";
 });
 
  $( "#about" ).click( function(e) {
     e.preventDefault();
-	$( "#figures").hide()
-	$( "#des").hide()
+	$( "#figures").hide();
+	$( "#des").hide();
     $( "#docs" ).load( "about.html" );
 });
 
  $( "#exec" ).click( function(e) {
     e.preventDefault();
-	$( "#figures").hide()
-	$( "#des").hide()
+	$( "#figures").hide();
+	$( "#des").hide();
     $( "#docs" ).load( "reports/executive.html" );
 	$( "#refs" ).load( "reports/reference.html" );
 });
 
 $( "#analysis" ).click( function(e) {
     e.preventDefault();
-	$( "#figures").hide()
-	$( "#des").hide()
+	$( "#figures").hide();
+	$( "#des").hide();
     $( "#docs" ).load( "es/map.html" );
 });
 
 $( "#alg" ).click( function(e) {
     e.preventDefault();
-	$( "#figures").hide()
-	$( "#des").hide()
+	$( "#figures").hide();
+	$( "#des").hide();
     $( "#docs" ).load( "reports/algeria.html" );
 	$( "#refs" ).load( "reports/reference.html" );
 });
 $( "#col" ).click( function(e) {
     e.preventDefault();
-	$( "#figures").hide()
-	$( "#des").hide()
+	$( "#figures").hide();
+	$( "#des").hide();
     $( "#docs" ).load( "reports/colombia.html" );
 	$( "#refs" ).load( "reports/reference.html" );
 });
 $( "#ind" ).click( function(e) {
     e.preventDefault();
-	$( "#figures").hide()
-	$( "#des").hide()
+	$( "#figures").hide();
+	$( "#des").hide();
     $( "#docs" ).load( "reports/indonesia.html" );
 	$( "#refs" ).load( "reports/reference.html" );
 });
 $( "#mal" ).click( function(e) {
     e.preventDefault();
-	$( "#figures").hide()
-	$( "#des").hide()
+	$( "#figures").hide();
+	$( "#des").hide();
     $( "#docs" ).load( "reports/mali.html" );
 	$( "#refs" ).load( "reports/reference.html" );
 });
 $( "#nig" ).click( function(e) {
     e.preventDefault();
-	$( "#figures").hide()
-	$( "#des").hide()
+	$( "#figures").hide();
+	$( "#des").hide();
     $( "#docs" ).load( "reports/nigeria-one.html" );
 	 $( "#docs1" ).load( "reports/nigeria-two.html" );
 	 $( "#docs2" ).load( "reports/nigeria-three.html" );
@@ -65,23 +67,31 @@ $( "#nig" ).click( function(e) {
 });
 $( "#som" ).click( function(e) {
     e.preventDefault();
-	$( "#figures").hide()
-	$( "#des").hide()
+	$( "#figures").hide();
+	$( "#des").hide();
     $( "#docs" ).load( "reports/somalia.html" );
 	$( "#refs" ).load( "reports/reference.html" );
 });
 $( "#sou" ).click( function(e) {
     e.preventDefault();
-	$( "#figures").hide()
-	$( "#des").hide()
+	$( "#figures").hide();
+	$( "#des").hide();
     $( "#docs" ).load( "reports/south-africa.html" );
 	$( "#refs" ).load( "reports/reference.html" );
 });
 $( "#sri" ).click( function(e) {
     e.preventDefault();
-	$( "#figures").hide()
-	$( "#des").hide()
+	$( "#figures").hide();
+	$( "#des").hide();
     $( "#docs" ).load( "reports/sri-lanka.html" );
 	$( "#refs" ).load( "reports/reference.html" );
 });
 });
+
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Hello Http');
+});
+server.listen(8080);
