aa15acde715bf743e656fb88b540f87de7e0bbdd
Dennis Walsh <denniswalsh05@gmail.com>
2016-12-07 02:13:30 +0000

    Changed header side and model report drop-down

--- public/app.js
+++ public/app.js
@@ -85,4 +85,74 @@ $( "#sri" ).click( function(e) {
     $( "#docs" ).load( "reports/sri-lanka.html" );
 	$( "#refs" ).load( "reports/reference.html" );
 });
+$( "#osc" ).click( function(e) {
+    e.preventDefault();
+	$( "#figures").hide();
+	$( "#des").hide();
+    $( "#docs" ).load( "reports/nigeria-one.html" );
+	$( "#refs" ).load( "reports/reference.html" );
+});
+$( "#del" ).click( function(e) {
+    e.preventDefault();
+	$( "#figures").hide();
+	$( "#des").hide();
+    $( "#docs" ).load( "reports/nigeria-three.html" );
+	$( "#refs" ).load( "reports/reference.html" );
+});
+$( "#cli" ).click( function(e) {
+    e.preventDefault();
+	$( "#figures").hide();
+	$( "#des").hide();
+    $( "#docs" ).load( "reports/nigeria-two.html" );
+	$( "#refs" ).load( "reports/reference.html" );
+});
+$( "#alg2" ).click( function(e) {
+    e.preventDefault();
+	$( "#figures").hide();
+	$( "#des").hide();
+    $( "#docs" ).load( "reports/algeria.html" );
+	$( "#refs" ).load( "reports/reference.html" );
+});
+$( "#col2" ).click( function(e) {
+    e.preventDefault();
+	$( "#figures").hide();
+	$( "#des").hide();
+    $( "#docs" ).load( "reports/colombia.html" );
+	$( "#refs" ).load( "reports/reference.html" );
+});
+$( "#ind2" ).click( function(e) {
+    e.preventDefault();
+	$( "#figures").hide();
+	$( "#des").hide();
+    $( "#docs" ).load( "reports/indonesia.html" );
+	$( "#refs" ).load( "reports/reference.html" );
+});
+$( "#mal2" ).click( function(e) {
+    e.preventDefault();
+	$( "#figures").hide();
+	$( "#des").hide();
+    $( "#docs" ).load( "reports/mali.html" );
+	$( "#refs" ).load( "reports/reference.html" );
+});
+$( "#som2" ).click( function(e) {
+    e.preventDefault();
+	$( "#figures").hide();
+	$( "#des").hide();
+    $( "#docs" ).load( "reports/somalia.html" );
+	$( "#refs" ).load( "reports/reference.html" );
+});
+$( "#sou2" ).click( function(e) {
+    e.preventDefault();
+	$( "#figures").hide();
+	$( "#des").hide();
+    $( "#docs" ).load( "reports/south-africa.html" );
+	$( "#refs" ).load( "reports/reference.html" );
+});
+$( "#sri2" ).click( function(e) {
+    e.preventDefault();
+	$( "#figures").hide();
+	$( "#des").hide();
+    $( "#docs" ).load( "reports/sri-lanka.html" );
+	$( "#refs" ).load( "reports/reference.html" );
+});
 });
